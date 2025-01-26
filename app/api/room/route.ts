38c591cc0ef944e88e050cf955c6fc204997a021
Gioi Nguyen <ncgioi.operor@gmail.com>
2025-01-26 15:56:49 +0700

    fix runtime

--- app/api/room/route.ts
+++ app/api/room/route.ts
@@ -1,7 +1,7 @@
 import dbConnect from "@/lib/mongo-connect";
 import lixi from "@/model/lixi.model";
 import { NextResponse } from "next/server";
-export const runtime = "edge";
+export const runtime = "nodejs";
 
 export async function POST(request: Request) {
   const body = await request.json();
